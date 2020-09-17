37aafd1ee4249f8f07c523842848ebde6803ddc6
Adalberto Junior <adalrsjr1@gmail.com>
2020-09-17 14:18:07 -0400

    update acmeair-monolith manifests to the current ST version

--- acmeair/build_openj9_k8s.sh
+++ acmeair/build_openj9_k8s.sh
@@ -1,3 +1,4 @@
 # Note: the IP of the mongo process is embedded into the image through mongo.properties file
-docker build -f Dockerfile_openj9_acmeair_k8s -t liberty-acmeair-k8s .
+docker build -f Dockerfile_openj9_acmeair_k8s -t smarttuning/liberty-acmeair-k8s .
+docker push smarttuning/liberty-acmeair-k8s
 
