f1d9d7ecd5465af2cf868ad81c4ebaa12f3ec54a
Adalberto Junior <adalrsjr1@gmail.com>
2020-11-22 16:33:17 -0500

    fix output format of best config found

--- optimization/smarttuning/bayesian.py
+++ optimization/smarttuning/bayesian.py
@@ -98,6 +98,8 @@ class BayesianEngine:
                     rstate=np.random.RandomState(random.randint(0, 1000)))
 
             best = partial_fmin()
+            best = space_eval(space, best)
+            logger.print(f'final best config: {best}')
             best.update({'is_best_config':True})
 
             logger.info(f'found best config after {max_evals} iterations: {best}')
